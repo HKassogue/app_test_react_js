6825c031bdb6f5e7fe36847fc4f2ffa8f1142493
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-09-27 00:26:13 +0000

    remove of the using variables

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const para = document.createElement('p');
-  para.textContent = "ceci est un paragraphe";
-  document.getElementById('root').append(para);
   return (
     <div>
       <h1>Cours React js</h1>
