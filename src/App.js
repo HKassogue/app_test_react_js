707be883f2e9bb7836b57fb65f05b91787a08978
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-09-27 01:36:23 +0000

    pass data via props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,61 @@ import './App.css';
 import DepenseItem from './components/DepenseItem';
 
 function App() {
+  const depenses = [
+    {
+      id: 'e1',
+      title: 'Papier toilette',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: 'e3',
+      title: 'Assurance voiture',
+      amount: 294.67,
+      date: new Date(2021, 2, 20),
+    },
+    {
+      id: 'e4',
+      title: 'PC',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+
   return (
     <div>
       <h1>Cours React js</h1>
       <h2>Let's started!</h2>
-      <DepenseItem></DepenseItem>
+      <DepenseItem
+        title = {depenses[0].title}
+        amount = {depenses[0].amount}
+        date = {depenses[0].date}
+      ></DepenseItem>
+
+      <DepenseItem
+        title = {depenses[1].title}
+        amount = {depenses[1].amount}
+        date = {depenses[1].date}
+      ></DepenseItem>
+
+      <DepenseItem
+        title = {depenses[2].title}
+        amount = {depenses[2].amount}
+        date = {depenses[2].date}
+      ></DepenseItem>
+
+      <DepenseItem
+        title = {depenses[3].title}
+        amount = {depenses[3].amount}
+        date = {depenses[3].date}
+      ></DepenseItem>
     </div>
   );
 }
--- src/components/DepenseItem.js
+++ src/components/DepenseItem.js
@@ -1,16 +1,13 @@
 import './DepenseItem.css'
 
-function DepenseItem() {
-    const DepenseDate = new Date(2022,3,28);
-    const DepenseTitre = "Assurance voiture";
-    const DepenseMontant = 290;
-
+function DepenseItem(props) {
+    
     return (
         <div className='depense-item'>
-            <div>{DepenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className='depense-item__description'>
-                <h2>{DepenseTitre}</h2>
-                <div className='depense-item__price'>{DepenseMontant} $</div>
+                <h2>{props.title}</h2>
+                <div className='depense-item__price'>{props.amount} $</div>
             </div>
         </div>
     );
