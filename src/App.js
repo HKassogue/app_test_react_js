e89188bb0aad8cdca4beefda9188422d228c7f4d
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-09-27 00:00:10 +0000

    display of hello word in index

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello word!</h1>
+        <p>Cours React js</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
