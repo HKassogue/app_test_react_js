03ec08d3ce12733bf1b5f4b90ce754f307820a04
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-09-27 00:31:32 +0000

    use of personnalized component in jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import DepenseItem from './components/DepenseItem';
 
 function App() {
   return (
     <div>
       <h1>Cours React js</h1>
       <h2>Let's started!</h2>
+      <DepenseItem></DepenseItem>
     </div>
   );
 }
---
+++ src/components/DepenseItem.js
@@ -0,0 +1,5 @@
+function DepenseItem() {
+    return <p>Depense Item!</p>
+}
+
+export default DepenseItem;
