57e4c42a20c481f0cf94280218614faaafb07a6c
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-10-04 17:30:03 +0000

    use of map (loop) for displaying depense items

--- src/components/expenses/Depenses.js
+++ src/components/expenses/Depenses.js
@@ -5,29 +5,13 @@ import './Depenses.css'
 const Depenses = (props) => {
     return (
         <div className='depenses'>
-          <DepenseItem
-            title = {props.depenses[0].title}
-            amount = {props.depenses[0].amount}
-            date = {props.depenses[0].date}
-          />
-    
-          <DepenseItem
-            title = {props.depenses[1].title}
-            amount = {props.depenses[1].amount}
-            date = {props.depenses[1].date}
-          />
-    
-          <DepenseItem
-            title = {props.depenses[2].title}
-            amount = {props.depenses[2].amount}
-            date = {props.depenses[2].date}
-          />
-    
-          <DepenseItem
-            title = {props.depenses[3].title}
-            amount = {props.depenses[3].amount}
-            date = {props.depenses[3].date}
-          />
+          {
+            props.depenses.map((depense, i) => <DepenseItem
+              title = {depense.title}
+              amount = {depense.amount}
+              date = {depense.date} />
+            )
+          }
         </div>
       );
 };
